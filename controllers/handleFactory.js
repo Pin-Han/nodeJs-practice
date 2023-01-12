b6e4896c792a24dac3557cf4ecb48c081de79176
Patrick <patrick04184@gmail.com>
2023-01-12 13:21:00 +0800

    calculate rating

--- controllers/handleFactory.js
+++ controllers/handleFactory.js
@@ -78,7 +78,9 @@ exports.getAll = Model =>
       .sort()
       .limitFields()
       .paginate();
-    const doc = await features.query.explain();
+    const doc = await features.query;
+    // const doc = await features.query.explain(); -> explain to expose more constructor
+    //
 
     // SEND RESPONSE
     res.status(200).json({
--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,6 +1,7 @@
 // review / rating / createdAt / ref to tour / ref to user
 
 const mongoose = require('mongoose');
+const Tour = require('./tourModel');
 
 const reviewSchema = new mongoose.Schema(
   {
@@ -47,6 +48,61 @@ reviewSchema.pre(/^find/, function(next) {
     select: 'name photo'
   });
   next();
+});
+// Statics are pretty much the same as methods but allow for defining functions that exist directly on your Model.
+// statics are the methods defined on the Model. methods are defined on the document (instance).
+reviewSchema.statics.calcAverageRating = async function(tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId }
+    },
+    {
+      $group: {
+        _id: '$tour',
+        nRating: { $sum: 1 },
+        avgRating: { $avg: '$rating' }
+      }
+    }
+  ]);
+  // console.log('stats', stats);
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: 0,
+      ratingsAverage: 4.5 //default
+    });
+  }
+};
+
+// Middleware can't call next post => middle
+reviewSchema.post('save', function() {
+  // This points to current review
+  // this.constructor ->
+  // console.log('save', this.constructor);
+  this.constructor.calcAverageRating(this.tour);
+  console.log('middle post ', this.constructor);
+});
+
+// Run before save a document in DB -> PRE
+// Run after save a document in DB -> POST
+reviewSchema.pre(/^findOneAnd/, async function(next) {
+  // console.log('Run pre findOneAnd');
+
+  this.r = await this.findOne();
+  // console.log('Run pre', this.r);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function() {
+  // await this.findOne(); does NOT work here, query has already executed
+  // console.log('Run post(Before)', this.r.constructor);
+
+  await this.r.constructor.calcAverageRating(this.r.tour);
+  // console.log('Run post', this.r);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
