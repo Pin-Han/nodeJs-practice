d48116425223ceeb85202f18e69ba0eee354d657
Patrick <patrick04184@gmail.com>
2023-01-26 11:52:14 +0800

    build landing page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,9 +1,17 @@
-exports.getOverview = (req, res) => {
-  res.status(200).render('base', {
-    tour: 'The Forest Hiker',
-    user: 'Jonas'
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1) get tour data from collection
+  const tours = await Tour.find();
+  // 2) Build template
+
+  //3) Render that template using tour data from 1)
+  res.status(200).render('overview', {
+    title: 'All Tours',
+    tours
   });
-};
+});
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour'
