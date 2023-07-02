bd1dca71366f8150dcbc4eafb6740ab30482a8b3
Patrick <patrick04184@gmail.com>
2023-07-02 15:45:55 +0800

    lesson 193 error page

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -5547,7 +5547,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53544" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60511" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
