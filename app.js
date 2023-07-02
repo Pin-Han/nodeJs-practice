ddb7265a5ed5c95c98c6dc7afad6d2ee245fde06
Patrick <patrick04184@gmail.com>
2023-07-02 17:48:38 +0800

    lesson 195 update user data

--- app.js
+++ app.js
@@ -58,6 +58,7 @@ app.use('/api', limiter);
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extends: true, limit: '10kb' }));
 app.use(cookieParser());
 // Data sanitization against NOSQL query injection
 app.use(mongoSanitize());
--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,13 @@
 /*eslint-disable */
 // import '@babel/polyfill';
 import { login, logout } from './login';
-
+import { updateData } from './updateSettings';
 // DOM elements
 const mapBox = document.getElementById('map');
-const loginForm = document.querySelector('.form');
+const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
+const userDataFrom = document.querySelector('.form-user-data');
+
 // Values
 
 // Delegation
@@ -24,6 +26,15 @@ if (loginForm) {
     login(email, password);
   });
 }
-if(logoutBtn){
-  logoutBtn.addEventListener('click',logout)
+if (logoutBtn) {
+  logoutBtn.addEventListener('click', logout);
+}
+
+if (userDataFrom) {
+  userDataFrom.addEventListener('submit', e => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+    updateData(name, email);
+  });
 }
